aaf3f6e03c747cdd22c77044065fd0c3736783f5
Nico Aleman <NicoAleman1@hotmail.com>
2022-11-19 10:41:23 -0500

    Updated compressed Parameter files to reflect removal of Multi-ESC
    Had to generate confxml files through VESCTool's built in parameter editor

--- balance/balance/conf/confparser.h
+++ balance/balance/conf/confparser.h
@@ -8,7 +8,7 @@
 #include <stdbool.h>
 
 // Constants
-#define BALANCE_CONFIG_SIGNATURE		4098546538
+#define BALANCE_CONFIG_SIGNATURE		3038062682
 
 // Functions
 int32_t confparser_serialize_balance_config(uint8_t *buffer, const balance_config *conf);
--- balance/balance/conf/confxml.h
+++ balance/balance/conf/confxml.h
@@ -8,7 +8,7 @@
 #include <stdbool.h>
 
 // Constants
-#define DATA_BALANCE_CONFIG__SIZE		6680
+#define DATA_BALANCE_CONFIG__SIZE		6098
 
 // Variables
 extern uint8_t data_balance_config_[];
