8d4f42f06ce6c80d3b2d6f5ba28072dfd3eaa8b6
Dado Mista <dadomista@gmail.com>
2023-10-18 12:34:41 -0700

    BMS Tiltback based on state!
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- float/float/float.c
+++ float/float/float.c
@@ -129,7 +129,8 @@ typedef struct {
 
 	// BMS
 	bool is_bms_supported;
-	
+	bool allow_bms_tiltback;
+
 	// Config values
 	float loop_time_seconds;
 	unsigned int start_counter_clicks, start_counter_clicks_max;
@@ -538,6 +539,7 @@ static void configure(data *d) {
 	d->do_handtest = false;
 
 	d->is_bms_supported = VESC_IF->mc_fault_to_string(FAULT_CODE_DUMMY_FEATURE_CHECK)[0] == 'B';
+	d->allow_bms_tiltback = true;
 }
 
 static void reset_vars(data *d) {
@@ -697,6 +699,7 @@ static float get_setpoint_adjustment_step_size(data *d) {
 			return d->tiltback_duty_step_size;
 		case (TILTBACK_HV):
 		case (TILTBACK_TEMP):
+		case (TILTBACK_BMS):
 			return d->tiltback_hv_step_size;
 		case (TILTBACK_LV):
 			return d->tiltback_lv_step_size;
@@ -994,11 +997,16 @@ static void calculate_setpoint_target(data *d) {
 		}
 		d->setpointAdjustmentType = TILTBACK_DUTY;
 		d->state = RUNNING_TILTBACK;
-	} else if (d->is_bms_supported && (VESC_IF->bms_get_fault_state() != BMS_FAULT_CODE_NONE)) {
+	} else if (d->is_bms_supported && d->allow_bms_tiltback && (VESC_IF->bms_get_fault_state() != BMS_FAULT_CODE_NONE)) {
 		d->beep_reason = BEEP_BMS;
 		beep_alert(d, 5, false);
 		d->setpointAdjustmentType = TILTBACK_BMS;
 		d->state = RUNNING_TILTBACK;
+		if(d->erpm > 0){
+			d->setpoint_target = d->float_conf.tiltback_lv_angle;
+		} else {
+			d->setpoint_target = -d->float_conf.tiltback_lv_angle;
+		}
 	} else if (d->abs_duty_cycle > 0.05 && input_voltage > d->float_conf.tiltback_hv) {
 		d->beep_reason = BEEP_HV;
 		beep_alert(d, 3, false);	// Triple-beep
@@ -2497,6 +2505,7 @@ static void send_realtime_data(data *d){
 	buffer_append_float32_auto(send_buffer, d->throttle_val, &ind);
 
 	// BMS
+	VESC_IF->printf("BMS State/Fault = %d/%d\n", VESC_IF->bms_get_op_state(), VESC_IF->bms_get_fault_state());
 	if (d->is_bms_supported) {
 		send_buffer[ind++] = VESC_IF->bms_get_fault_state();
 		send_buffer[ind++] = VESC_IF->bms_get_op_state();
@@ -2905,6 +2914,12 @@ static void cmd_runtime_tune_tilt(data *d, unsigned char *cfg, int len)
 {
 	unsigned int flags = cfg[0];
 	bool duty_buzz = flags & 0x1;
+	bool control_bms_tilt = flags & 0x2;
+	if (control_bms_tilt) {
+		d->allow_bms_tiltback = flags & 0x4;
+		beep_alert(d, 1, false);
+		return;
+	}
 	d->float_conf.is_dutybuzz_enabled = duty_buzz;
 	float retspeed = cfg[1];
 	if (retspeed > 0) {
@@ -3481,7 +3496,7 @@ static void stop(void *arg) {
 INIT_FUN(lib_info *info) {
 	INIT_START
 	if (!VESC_IF->app_is_output_disabled()) {
-		VESC_IF->printf("Init Float v%.1fd\n", (double)APPCONF_FLOAT_VERSION);
+		VESC_IF->printf("Init Float v%.1flog\n", (double)APPCONF_FLOAT_VERSION);
 	}
 
 	data *d = VESC_IF->malloc(sizeof(data));
