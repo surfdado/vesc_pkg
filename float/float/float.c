8591bbc7ea22074e1915511feb8d3a477abcdd42
Lukáš Hrázký <lukkash@email.cz>
2023-11-19 14:53:36 +0100

    Move footpad sensor code to its own module

--- float/float/float.c
+++ float/float/float.c
@@ -20,6 +20,8 @@
 
 #include "vesc_c_if.h"
 
+#include "footpad_sensor.h"
+
 #include "conf/datatypes.h"
 #include "conf/confparser.h"
 #include "conf/confxml.h"
@@ -88,18 +90,6 @@ typedef enum {
 	TILTBACK_LV,
 	TILTBACK_TEMP
 } SetpointAdjustmentType;
-
-typedef enum {
-	FS_NONE = 0,
-	FS_LEFT = 1,
-	FS_RIGHT = 2,
-	FS_BOTH = 3
-} FootpadSensorState;
-
-typedef struct {
-	float adc1, adc2;
-	FootpadSensorState state;
-} FootpadSensor;
 
 typedef struct{
 	float a0, a1, a2, b1, b2;
@@ -670,59 +660,6 @@ static float get_setpoint_adjustment_step_size(data *d) {
 			;
 	}
 	return 0;
-}
-
-// Read ADCs and determine footpad sensor state
-static FootpadSensorState footpad_sensor_state_evaluate(const FootpadSensor *fs, const float_config *config) {
-	// Calculate sensor state from ADC values
-	if (config->fault_adc1 == 0 && config->fault_adc2 == 0) { // No sensors
-		return FS_BOTH;
-	} else if (config->fault_adc2 == 0) { // Single sensor on ADC1
-		if (fs->adc1 > config->fault_adc1) {
-			return FS_BOTH;
-		}
-	} else if (config->fault_adc1 == 0) { // Single sensor on ADC2
-		if (fs->adc2 > config->fault_adc2) {
-			return FS_BOTH;
-		}
-	} else { // Double sensor
-		if (fs->adc1 > config->fault_adc1) {
-			if (fs->adc2 > config->fault_adc2) {
-				return FS_BOTH;
-			} else {
-				return FS_LEFT;
-			}
-		} else {
-			if (fs->adc2 > config->fault_adc2) {
-				return FS_RIGHT;
-			}
-		}
-	}
-
-	return FS_NONE;
-}
-
-static void footpad_sensor_update(FootpadSensor *fs, const float_config *config) {
-	fs->adc1 = VESC_IF->io_read_analog(VESC_PIN_ADC1);
-	fs->adc2 = VESC_IF->io_read_analog(VESC_PIN_ADC2); // Returns -1.0 if the pin is missing on the hardware
-	if (fs->adc2 < 0.0) {
-		fs->adc2 = 0.0;
-	}
-
-	fs->state = footpad_sensor_state_evaluate(fs, config);
-}
-
-static int footpad_sensor_state_to_switch_compat(FootpadSensorState v) {
-	switch (v) {
-	case FS_BOTH:
-		return 2;
-	case FS_LEFT:
-	case FS_RIGHT:
-		return 1;
-	case FS_NONE:
-	default:
-		return 0;
-	}
 }
 
 bool is_engaged(const data *d) {
